076bedf40803d0341f9bd8d58c4f29fa039cb44b
Tom Kent <email@tomkent.me>
2017-07-19 22:36:20 +0100

    New .NET target

--- ColonyPlusPlus/ColonyPlusPlus/ColonyPlusPlus.cs
+++ ColonyPlusPlus/ColonyPlusPlus/ColonyPlusPlus.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Pipliz;
+using Pipliz.Chatting;
+using Pipliz.JSON;
+using Pipliz.Threading;
+
 
 namespace ColonyPlusPlus
 {
+    [ModLoader.ModManager]
     public class ColonyPlusPlus
     {
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterAddingBaseTypes)]
+        public static void AfterAddingBaseTypes()
+        {
+            /*ItemTypesServer.AddTextureMapping("ExampleBlock1", new JSONNode(NodeType.Object)
+                .SetAs("albedo", "grassTemperate")
+                .SetAs("normal", "stoneblock")
+                .SetAs("emissive", "ovenLitFront")
+                .SetAs("height", "oreCoal")
+            );
+
+            ItemTypesServer.AddTextureMapping("ExampleBlock2", new JSONNode(NodeType.Object)
+                .SetAs("albedo", "grindstone")
+                .SetAs("normal", "berrybush")
+                .SetAs("emissive", "torch")
+                .SetAs("height", "snow")
+            );*/
+
+            // Register types
+            classes.Typemanager.registerTypes();
+
+
+        }
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterItemTypesServer)]
+        public static void AfterItemTypesServer()
+        {
+
+            classes.Typemanager.registerTrackedTypes();
+        }
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterWorldLoad)]
+        public static void AfterWorldLoad()
+        {
+
+            //Stockpile.AddToInitialPile()
+        }
     }
 }
