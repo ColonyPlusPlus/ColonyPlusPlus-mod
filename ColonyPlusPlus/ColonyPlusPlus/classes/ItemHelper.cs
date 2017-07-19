4a5de32eb402b50cfb76827586a228f6b68980da
Tim Greason <greasontim@gmail.com>
2017-07-19 18:06:00 -0400

    Added ItemHelper

---
+++ ColonyPlusPlus/ColonyPlusPlus/classes/ItemHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ColonyPlusPlus.classes
+{
+    class ItemHelper
+    {
+        public struct OnRemove
+        {
+            private string type;
+            private int amount;
+            private float chance;
+            public OnRemove(string type, int amount, float chance)
+            {
+                this.type = type;
+                this.amount = amount;
+                this.chance = chance;
+            }
+            public string GetType()
+            {
+                return type;
+            }
+            public int GetAmount()
+            {
+                return amount;
+            }
+            public float GetChance()
+            {
+                return chance;
+            }
+        }
+    }
+}
