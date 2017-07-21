d321b0b7bc8a723952e4427d6d5ce02a064f71b5
Tom Kent <email@tomkent.me>
2017-07-21 18:18:47 +0100

    Added welcome message

--- ColonyPlusPlus/ColonyPlusPlus/ColonyPlusPlus.cs
+++ ColonyPlusPlus/ColonyPlusPlus/ColonyPlusPlus.cs
@@ -3,17 +3,24 @@ using Pipliz.Chatting;
 using Pipliz.JSON;
 using Pipliz.Threading;
 using ColonyPlusPlus;
+using static Players;
 
 namespace ColonyPlusPlus
 {
     [ModLoader.ModManager]
     public class ColonyPlusPlus
     {
-        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterAssemblyLoad)]
-        public static void AfterAssemblyLoad()
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterStartup)]
+        public static void AfterStartup()
         {
             Pipliz.Log.Write("Loaded ColonyPlusPlus v0.0.14");
             
+        }
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnPlayerConnected)]
+        public static void OnPlaterConnected(Player p)
+        {
+            Chat.Send(p.ID, "Welcome to ColonyPlusPlus, the first mod for Colony Survival to implement the new Modding API. If you have any questions please see our Steam Forum thread or the GitHub repository.");
         }
 
         [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterAddingBaseTypes)]
