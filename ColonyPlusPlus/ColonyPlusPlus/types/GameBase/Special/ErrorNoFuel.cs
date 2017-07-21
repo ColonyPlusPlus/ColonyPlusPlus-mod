21eaf1dd6f0cc150b872a88299742772035a06d5
Tim Greason <greasontim@gmail.com>
2017-07-21 12:36:12 -0400

    Added Errors

---
+++ ColonyPlusPlus/ColonyPlusPlus/types/GameBase/Special/ErrorNoFuel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ColonyPlusPlus.types.GameBase.Special
+{
+    class ErrorNoFuel : classes.Type
+    {
+        public ErrorNoFuel(string name) : base(name)
+        {
+            this.ParentType = "missingerror";
+            this.icon = "errorNoFuel.png";
+            this.Register();
+        }
+    }
+}
