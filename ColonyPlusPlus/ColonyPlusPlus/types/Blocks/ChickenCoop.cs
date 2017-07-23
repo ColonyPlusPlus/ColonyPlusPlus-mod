bc567b644bd3e9d26e10fede946f2e0782722f6e
Tim Greason <greasontim@gmail.com>
2017-07-23 17:52:07 -0400

    Update to chickencoop

--- ColonyPlusPlus/ColonyPlusPlus/types/Blocks/ChickenCoop.cs
+++ ColonyPlusPlus/ColonyPlusPlus/types/Blocks/ChickenCoop.cs
@@ -1,4 +1,5 @@
-﻿using ColonyPlusPlus.classes.Managers;
+﻿using ColonyPlusPlus.classes;
+using ColonyPlusPlus.classes.Managers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,12 +19,23 @@ namespace ColonyPlusPlus.types.blocks
             this.SideAll = "planks";
             this.IsPlaceable = true;
 
+            CustomDataItem[] customData = { new CustomDataItem("minerIsMineable", true), new CustomDataItem("minerMiningTime", true) };
+            CustomDataHelper c = new CustomDataHelper(customData);
+            this.CustomData = c.customDataNode;
+
+            classes.ItemHelper.OnRemove[] onRemoveNode = {
+                new classes.ItemHelper.OnRemove("feather",    2,  1.0f),
+                new classes.ItemHelper.OnRemove("feather",    1,  0.55f),
+                new classes.ItemHelper.OnRemove("egg",        1,  0.2f)
+            };
+            this.OnRemove = onRemoveNode;
+
             this.Register();
         }
 
         public override void AddRecipes()
         {
-            /*RecipeManager.AddRecipe("crafting",
+            RecipeManager.AddRecipe("crafting",
                 new List<InventoryItem> {
                     RecipeManager.Item("logtemperate", 1),
                     RecipeManager.Item("planks", 4),
@@ -32,7 +44,7 @@ namespace ColonyPlusPlus.types.blocks
                 new List<InventoryItem> {
                     RecipeManager.Item("chickencoop", 1)
                 },
-                0.0f);*/
+                0.0f);
         }
     }
 }
