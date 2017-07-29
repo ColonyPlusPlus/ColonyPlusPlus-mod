ce42f2d17292840b66098241be589ca0a65191b9
Tim Greason <greasontim@gmail.com>
2017-07-29 10:43:51 -0400

    Starting making all the base game mats into code instead of the .json file.

---
+++ ColonyPlusPlus/ColonyPlusPlus/Classes/Managers/BaseGameMaterialManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ColonyPlusPlus.Classes.Managers
+{
+    class BaseGameMaterialManager
+    {
+        public static void initialiseMaterials()
+        {
+            Material.createMaterial("grasstemperate", "grasstemperate", "neutral", "grassGeneric", "grassGeneric");
+            Material.createMaterial("grasstundra", "grasstundra", "neutral", "grassGeneric", "grassGeneric");
+            Material.createMaterial("grasstaiga", "grasstaiga", "neutral", "grassGeneric", "grassGeneric");
+            Material.createMaterial("grasssavanna", "grasssavanna", "neutral", "grassGeneric", "grassGeneric");
+            Material.createMaterial("grassrainforest", "grassrainforest", "neutral", "grassGeneric", "grassGeneric");
+        }
+    }
+}
