db3eeaa99c84e04701f764ddb009b60a9b1ce5a4
Tom Kent <email@tomkent.me>
2017-07-30 17:12:52 +0100

    Added colourful chat commands

--- ColonyPlusPlus/ColonyPlusPlus/Classes/ChatCommands/ChunkCommands.cs
+++ ColonyPlusPlus/ColonyPlusPlus/Classes/ChatCommands/ChunkCommands.cs
@@ -4,7 +4,7 @@ using ChatCommands;
 using NPC;
 using Permissions;
 using Pipliz;
-using Pipliz.Chatting;
+using ColonyPlusPlus.Classes.Helpers;
 using System;
 
 namespace ColonyPlusPlus.Classes.ChatCommands
@@ -36,7 +36,7 @@ namespace ColonyPlusPlus.Classes.ChatCommands
                     }
                 } else
                 {
-                    Chat.Send(id, "Correct usage: /chunk {action} where action can be claim, unclaim, or delete", ChatSenderType.Server);
+                    Chat.send(id, "Correct usage: /chunk {action} where action can be claim, unclaim, or delete", Chat.ChatColour.lime, Chat.ChatStyle.bold);
                 }
                 
             }
@@ -61,13 +61,13 @@ namespace ColonyPlusPlus.Classes.ChatCommands
                     Vector3Int chunkPos = position.ToChunk();
                     int owned = Managers.WorldManager.getOwnedChunkCount(p.ID);
 
-                    Chat.Send(id, String.Format("Claimed chunk: {0}, {1}, {2}", chunkPos.x, chunkPos.y, chunkPos.z), ChatSenderType.Server);
-                    Chat.Send(id, String.Format("You now own {0} chunks.", owned), ChatSenderType.Server);
+                    Chat.send(id, String.Format("Claimed chunk: {0}, {1}, {2}", chunkPos.x, chunkPos.y, chunkPos.z), Chat.ChatColour.lime, Chat.ChatStyle.bold);
+                    Chat.send(id, String.Format("You now own {0} chunks.", owned), Chat.ChatColour.lime, Chat.ChatStyle.bold);
                 }
                 else
                 {
                     // chunk already owned
-                    Chat.Send(id, "Unable to claim chunk", ChatSenderType.Server);
+                    Chat.send(id, "Unable to claim chunk", Chat.ChatColour.red, Chat.ChatStyle.bold);
                 }
 
 
@@ -96,13 +96,13 @@ namespace ColonyPlusPlus.Classes.ChatCommands
                     Vector3Int chunkPos = position.ToChunk();
                     int owned = Managers.WorldManager.getOwnedChunkCount(p.ID);
 
-                    Chat.Send(id, String.Format("Unclaimed chunk: {0}, {1}, {2}", chunkPos.x, chunkPos.y, chunkPos.z), ChatSenderType.Server);
-                    Chat.Send(id, String.Format("You now own {0} chunks.", owned), ChatSenderType.Server);
+                    Chat.send(id, String.Format("Unclaimed chunk: {0}, {1}, {2}", chunkPos.x, chunkPos.y, chunkPos.z), Chat.ChatColour.lime, Chat.ChatStyle.bold);
+                    Chat.send(id, String.Format("You now own {0} chunks.", owned), Chat.ChatColour.lime, Chat.ChatStyle.bold);
                 }
                 else
                 {
                     // chunk already owned
-                    Chat.Send(id, "Unable to unclaim chunk", ChatSenderType.Server);
+                    Chat.send(id, "Unable to unclaim chunk", Chat.ChatColour.red, Chat.ChatStyle.bold);
                 }
 
 
@@ -131,13 +131,13 @@ namespace ColonyPlusPlus.Classes.ChatCommands
                     Vector3Int chunkPos = position.ToChunk();
                     int owned = Managers.WorldManager.getOwnedChunkCount(p.ID);
 
-                    Chat.Send(id, String.Format("Unclaimed chunk: {0}, {1}, {2}", chunkPos.x, chunkPos.y, chunkPos.z), ChatSenderType.Server);
-                    Chat.Send(id, String.Format("You now own {0} chunks.", owned), ChatSenderType.Server);
+                    Chat.send(id, String.Format("Unclaimed chunk: {0}, {1}, {2}", chunkPos.x, chunkPos.y, chunkPos.z), Chat.ChatColour.lime, Chat.ChatStyle.bold);
+                    Chat.send(id, String.Format("You now own {0} chunks.", owned), Chat.ChatColour.lime, Chat.ChatStyle.bold);
                 }
                 else
                 {
                     // chunk already owned
-                    Chat.Send(id, "Unable to unclaim chunk", ChatSenderType.Server);
+                    Chat.send(id, "Unable to unclaim chunk", Chat.ChatColour.red, Chat.ChatStyle.bold);
                 }
 
 
