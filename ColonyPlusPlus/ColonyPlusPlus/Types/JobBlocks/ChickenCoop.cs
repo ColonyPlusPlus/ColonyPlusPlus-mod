8222025f0929f46d19b92c7c95f4f0e991b9084e
Tom Kent <email@tomkent.me>
2017-08-05 22:36:16 +0100

    Added basic chickencoop jobblock

---
+++ ColonyPlusPlus/ColonyPlusPlus/Types/JobBlocks/ChickenCoop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ColonyPlusPlus.Types.JobBlocks
+{
+    class ChickenCoop : Classes.Type
+    {
+        public ChickenCoop(string name) : base(name)
+        {
+            this.OnRemoveAudio = "woodDeleteLight";
+            this.OnPlaceAudio = "woodPlace";
+            this.SideAll = "planks";
+            this.NPCLimit = 0;
+            this.IsPlaceable = true;
+            this.Register();
+        }
+    }
+}
