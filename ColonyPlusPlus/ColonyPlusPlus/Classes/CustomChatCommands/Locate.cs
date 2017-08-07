894fcb75d03462a5a02399ff4ac03dd11467e862
bernhardsson.philip <bernhardsson.philip@gmail.com>
2017-08-07 18:36:31 +0200

    Added permission to locate command

--- ColonyPlusPlus/ColonyPlusPlus/Classes/CustomChatCommands/Locate.cs
+++ ColonyPlusPlus/ColonyPlusPlus/Classes/CustomChatCommands/Locate.cs
@@ -1,4 +1,5 @@
-﻿using Pipliz.Chatting;
+﻿using Permissions;
+using Pipliz.Chatting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,14 @@ namespace ColonyPlusPlus.Classes.CustomChatCommands
 
         protected override bool RunCommand(Players.Player ply, string[] args, NetworkID target)
         {
-            var player = Players.GetPlayer(target);
-            var status = player.IsConnected ? "online" : "offline";
-            Chat.Send(ply, $"{player.Name} is {status} and was last seen at x:{player.Position.x} y:{player.Position.y} z:{player.Position.z}");
+            if (PermissionsManager.CheckAndWarnPermission(ply, "locate"))
+            {
+                var player = Players.GetPlayer(target);
+                var status = player.IsConnected ? "online" : "offline";
+                Chat.Send(ply,
+                    $"{player.Name} is {status} and was last seen at x:{player.Position.x} y:{player.Position.y} z:{player.Position.z}");
+            }
+
             return true;
         }
     }
@@ -29,17 +35,19 @@ namespace ColonyPlusPlus.Classes.CustomChatCommands
 
         protected override bool RunCommand(Players.Player ply, string[] args, NetworkID target)
         {
-            var player = Players.GetPlayer(target);
+            if (PermissionsManager.CheckAndWarnPermission(ply, "locate"))
+            {
+                var player = Players.GetPlayer(target);
 
-            var banner = (from b in BannerTracker.GetBanners()
-                where b.Owner == player
-                select b).FirstOrDefault();
+                var banner = (from b in BannerTracker.GetBanners()
+                    where b.Owner == player
+                    select b).FirstOrDefault();
 
-            Chat.Send(ply,
-                banner != null
-                    ? $"{player.Name} has a banner at x:{banner.KeyLocation.x} y:{banner.KeyLocation.y} z:{banner.KeyLocation.z}"
-                    : $"{player.Name} doesnt have a banner");
-
+                Chat.Send(ply,
+                    banner != null
+                        ? $"{player.Name} has a banner at x:{banner.KeyLocation.x} y:{banner.KeyLocation.y} z:{banner.KeyLocation.z}"
+                        : $"{player.Name} doesnt have a banner");
+            }
             return true;
         }
     }
