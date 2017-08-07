428de0dcb6af52515ead0da6a7ba4b4fdc7aa43a
bernhardsson.philip <bernhardsson.philip@gmail.com>
2017-08-07 22:55:42 +0200

    Added unban command

--- ColonyPlusPlus/ColonyPlusPlus/Classes/CustomChatCommands/Ban.cs
+++ ColonyPlusPlus/ColonyPlusPlus/Classes/CustomChatCommands/Ban.cs
@@ -26,4 +26,23 @@ namespace ColonyPlusPlus.Classes.CustomChatCommands
             return true;
         }
     }
+
+    public class Unban : BaseChatCommand
+    {
+        public Unban() : base("/unban", true)
+        {
+        }
+
+        protected override bool RunCommand(Players.Player ply, string[] args, NetworkID target)
+        {
+            if (PermissionsManager.CheckAndWarnPermission(ply, "unban"))
+            {
+                //TODO: Log unbans
+                var targetPlayer = Players.GetPlayer(target);
+                BlackAndWhitelisting.RemoveBlackList(targetPlayer.ID.steamID.m_SteamID);
+                Chat.Send(ply, $"Unbanned {targetPlayer.Name}");
+            }
+            return true;
+        }
+    }
 }
