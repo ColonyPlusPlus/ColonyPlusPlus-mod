7a0a165d370b4f33719f948d753f3c56a6ec38d0
Tom Kent <email@tomkent.me>
2017-08-11 20:43:48 +0100

    Updated material manager

--- ColonyPlusPlus/ColonyPlusPlus/Classes/Managers/MaterialManager.cs
+++ ColonyPlusPlus/ColonyPlusPlus/Classes/Managers/MaterialManager.cs
@@ -7,42 +7,42 @@
         /// </summary>
         public static void initialiseMaterials()
         {
-            Material.createMaterial("wildberrybush", "wildberrybush", "neutral", "berrybush", "berrybush");
-            Material.createMaterial("vegetablepatch", "leavesTemperate", "neutral", "leavesTemperate", "leavesTemperate");
-            Material.createMaterial("sugarcane", "clayblocklime", "neutral", "leavesTemperate", "leavesTemperate");
+            ColonyAPI.Managers.MaterialManager.createMaterial("wildberrybush", "wildberrybush", "neutral", "berrybush", "berrybush");
+            ColonyAPI.Managers.MaterialManager.createMaterial("vegetablepatch", "leavesTemperate", "neutral", "leavesTemperate", "leavesTemperate");
+            ColonyAPI.Managers.MaterialManager.createMaterial("sugarcane", "clayblocklime", "neutral", "leavesTemperate", "leavesTemperate");
 
 
-            Material.createMaterial("cpplogtemperatetop", "cpplogtemperatetop", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("cpplogtemperate", "cpplogtemperate", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("cpplogtaiga", "cpplogtaiga", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("cpplogtaigatop", "cpplogtaigatop", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("cpplogtemperatetop", "cpplogtemperatetop", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("cpplogtemperate", "cpplogtemperate", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("cpplogtaiga", "cpplogtaiga", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("cpplogtaigatop", "cpplogtaigatop", "neutral", "plasterblock", "plasterblock");
 
             // job stuff
-            Material.createMaterial("welltop", "well", "neutral", "well", "plasterblock");
-            Material.createMaterial("masontable", "masontable", "neutral", "masontable", "plasterblock");
-            Material.createMaterial("carpentrytable", "carpentrytable", "neutral", "carpentrytable", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("welltop", "well", "neutral", "well", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("masontable", "masontable", "neutral", "masontable", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("carpentrytable", "carpentrytable", "neutral", "carpentrytable", "plasterblock");
 
             // Decorative blocks
-            Material.createMaterial("clayblock", "clayblock", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockblack", "clayblockblack", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockblue", "clayblockblue", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockbrown", "clayblockbrown", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockcyan", "clayblockcyan", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockgray", "clayblockgray", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockgreen", "clayblockgreen", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblocklightblue", "clayblocklightblue", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblocklime", "clayblocklime", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockmagenta", "clayblockmagenta", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockorange", "clayblockorange", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockpink", "clayblockpink", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockpurple", "clayblockpurple", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockred", "clayblockred", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblocksilver", "clayblocksilver", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockwhite", "clayblockwhite", "neutral", "plasterblock", "plasterblock");
-            Material.createMaterial("clayblockyellow", "clayblockyellow", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblock", "clayblock", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockblack", "clayblockblack", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockblue", "clayblockblue", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockbrown", "clayblockbrown", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockcyan", "clayblockcyan", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockgray", "clayblockgray", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockgreen", "clayblockgreen", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblocklightblue", "clayblocklightblue", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblocklime", "clayblocklime", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockmagenta", "clayblockmagenta", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockorange", "clayblockorange", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockpink", "clayblockpink", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockpurple", "clayblockpurple", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockred", "clayblockred", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblocksilver", "clayblocksilver", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockwhite", "clayblockwhite", "neutral", "plasterblock", "plasterblock");
+            ColonyAPI.Managers.MaterialManager.createMaterial("clayblockyellow", "clayblockyellow", "neutral", "plasterblock", "plasterblock");
 
-            Material.createMaterial("marble", "graymarble", "neutral", "neutral", "neutral");
-            Material.createMaterial("grasstemperateside", "grassTemperateSide", "neutral", "grassGenericSide", "grassGenericSide");
+            ColonyAPI.Managers.MaterialManager.createMaterial("marble", "graymarble", "neutral", "neutral", "neutral");
+            ColonyAPI.Managers.MaterialManager.createMaterial("grasstemperateside", "grassTemperateSide", "neutral", "grassGenericSide", "grassGenericSide");
         }
     }
 }
